c07bf0d00fcfeefbbf8c11a33edee3ce730bb2f6
Mohamed-gulam <143926058+Mohamed-gulam@users.noreply.github.com>
2023-10-20 22:20:26 +0530

    Update jen.sh

--- jen.sh
+++ jen.sh
@@ -2,13 +2,4 @@
 #
 echo "This is shell script"i
 echo "second line added" 
-echo "auto triggred in jenk
-qwe
-wewe
-we2e
-2e2
-e2e
-2e2
-ins successfully now"
-  w  sqwd
-s  qs  q
+echo "auto triggred in jenk"
