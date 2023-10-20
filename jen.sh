11bc59bb4c2b2fbf08f02bf78337b09530ec2a54
mohamed-gulam <mohamedgulam1011@gmail.com>
2023-10-20 21:24:31 +0530

    new commit

--- jen.sh
+++ jen.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 #
-echo "This is shell script"
+echo "This is shell script"i
 echo "second line added" 
-echo "auto triggred in jenkins successfully now"
+echo "auto triggred in jenk
+qwe
+wewe
+we2e
+2e2
+e2e
+2e2
+ins successfully now"
